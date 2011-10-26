06297e9367443d5e96cb32660a199734b5790d2f
Wes Okes <wokes@lamppostgroup.com>
2011-10-26 11:45:38 -0500

    wow

--- public/modules/files/js/music/Player.js
+++ public/modules/files/js/music/Player.js
@@ -1,40 +1,102 @@
 Ext.define('Modules.files.js.music.Player', {
 	extend:'Ext.toolbar.Toolbar',
+	mixins: {
+		log: 'Redokes.debug.Log'
+	},
 	
 	lastVolume: 0,
 	isPaused: true,
+	isLoaded: false,
 	
 	initComponent: function(){
-		this.items = [];
+		this.showLog();
+		this.items = this.items || [];
+		this.initButtons();
 		this.callParent(arguments);
+	},
+	
+	initButtons: function() {
+		this.playButton = Ext.create('Ext.button.Button', {
+			text: 'Play',
+			scope: this,
+			handler: this.play
+		});
+		this.items.push(this.playButton);
+		
+		this.pauseButton = Ext.create('Ext.button.Button', {
+			text: 'Pause',
+			scope: this,
+			handler: this.pause
+		});
+		this.items.push(this.pauseButton);
+		
+		this.previousButton = Ext.create('Ext.button.Button', {
+			text: 'Previous',
+			scope: this,
+			handler: this.previous
+		});
+		this.items.push(this.previousButton);
+		
+		this.nextButton = Ext.create('Ext.button.Button', {
+			text: 'Next',
+			scope: this,
+			handler: this.next
+		});
+		this.items.push(this.nextButton);
+		
+		this.stopButton = Ext.create('Ext.button.Button', {
+			text: 'Stop',
+			scope: this,
+			handler: this.stop
+		});
+		this.items.push(this.stopButton);
+		
 	},
 	
 	setRawSrc: function(type, data){
 		this.audio.dom.src = 'data:' + type + ';base64,' + window.btoa(data);
 	},
 	
-	play: function(){
-		this.audio.dom.play();
-		this.isPaused = false;
+	play: function() {
+		this.log('Play');
+		if (this.isLoaded) {
+			this.audio.dom.play();
+			this.isPaused = false;
+		}
 	},
 	
 	stop: function() {
-		this.audio.dom.pause();
-		this.audio.dom.seek(0);
+		this.log('Stop');
+		if (this.isLoaded) {
+			this.audio.dom.pause();
+			this.seek(0);
+		}
 	},
 	
 	pause: function() {
-		if (this.isPaused) {
-			this.isPaused = false;
-			this.audio.dom.pause();
-		}
-		else {
-			this.isPaused = true;
-			this.audio.dom.play();
+		this.log('Pause');
+		if (this.isLoaded) {
+			if (this.isPaused) {
+				this.isPaused = false;
+				this.audio.dom.pause();
+			}
+			else {
+				this.isPaused = true;
+				this.audio.dom.play();
+			}
 		}
 	},
 	
+	previous: function() {
+		this.log('Previous');
+	},
+	
+	next: function() {
+		this.log('Next');
+	},
+	
 	setVolume: function(volume) {
+		this.log('Set volume ' + volume);
 		this.audio.dom.volume = volume;
 	},
 	
@@ -48,13 +110,15 @@ Ext.define('Modules.files.js.music.Player', {
 	},
 	
 	seek: function(seconds) {
-		this.audio.dom.currentTime = seconds;
+		if (this.isLoaded) {
+			this.audio.dom.currentTime = seconds;
+		}
 	},
 	
 	//Init Functions
 	onRender: function(){
 		this.callParent(arguments);
-		this.audio = this.body.createChild({
+		this.audio = Ext.getBody().createChild({
 			tag: 'audio',
 			controls: true
 		});
