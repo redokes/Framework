b49959602b0a786f042cbd862c42f2985a24e001
Wes Okes <wokes@lamppostgroup.com>
2011-11-18 16:08:57 -0500

    * Modified mercury editor to work inside at least one iframe

---
+++ public/modules/template/js/view/Edit.js
@@ -0,0 +1,77 @@
+Ext.define('Modules.template.js.Interface', {
+	extend: 'Redokes.tab.Interface',
+	
+	height: 400,
+	width: 600,
+	
+	tabTitleTemplate: '{title}',
+	tabIconCls: 'contact-icon-16',
+	tabClass: 'Modules.template.js.form.Template',
+	tabModel: 'Modules.template.js.model.Template',
+	addFormClass: 'Modules.template.js.form.Template',
+	
+	initComponent: function() {
+		this.items = this.items || [];
+		this.dockedItems = this.dockedItems || [];
+		this.init();
+		this.callParent(arguments);
+	},
+	
+	init: function() {
+		this.initTemplateView();
+		this.initListeners();
+	},
+	
+	initTemplateView: function() {
+		this.templateView = Ext.create('Modules.template.js.ViewPanel', {
+			title: 'Templates'
+		});
+		this.items.push(this.templateView);
+	},
+	
+	initListeners: function() {
+		this.templateView.dataView.on('itemdblclick', function(view, record, item, index) {
+			this.setActiveTab(this.createTab(record));
+		}, this);
+		
+		this.templateView.on('edit', function(view, records) {
+			var numRecords = records.length;
+			for (var i = 0; i < numRecords; i++) {
+				this.createTab(records[i]);
+			}
+		}, this);
+		
+		this.templateView.on('delete', function(view, records) {
+			var numRecords = records.length;
+			var ids = [];
+			for (var i = 0; i < numRecords; i++) {
+				ids.push(records[i].data.templateId);
+			}
+			Ext.Ajax.request({
+				scope: this,
+				method: 'delete',
+				url: '/template/rest/delete',
+				params: {
+					ids: Ext.encode(ids)
+				},
+				success: function(r) {
+					var response = Ext.decode(r.responseText);
+					this.reloadStore();
+				}
+			});
+		}, this);
+		
+		this.on('add', function() {
+			this.reloadStore();
+		}, this);
+		
+		this.on('edit', function() {
+			this.reloadStore();
+		}, this);
+	},
+	
+	reloadStore: function() {
+		this.templateView.dataView.store.load();
+	}
+	
+});
