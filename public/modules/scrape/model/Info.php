83b9a137dc2b5863d547e9748ad1361cb0a4f1ed
Wes Okes <wokes@lamppostgroup.com>
2011-12-01 17:32:28 -0500

    * Added scrape to tempalte support, but all references are messed up

--- public/modules/scrape/model/Info.php
+++ public/modules/scrape/model/Info.php
@@ -32,6 +32,7 @@ class Scrape_Model_Info extends Redokes_Model_Model {
 	public function afterUpdate() {
 		if ($this->row->complete) {
 			$this->createThumb();
+			$this->convertToTemplate();
 		}
 	}
 	
@@ -51,6 +52,79 @@ class Scrape_Model_Info extends Redokes_Model_Model {
 		if (!is_dir($scrapeDir)) {
 			mkdir($scrapeDir);
 			chmod($scrapeDir, 0777);
+		}
+	}
+	
+	/**
+	 *
+	 * @return Scrape_Model_Page $page
+	 */
+	public function getFirstPage() {
+		$page = new Scrape_Model_Page();
+		$select = $page->table->select()
+				->where('scrapeId = ?', $this->row->scrapeId)
+				->order('pageId')
+				->limit(1);
+		$row = $page->table->fetchRow($select);
+		$page->row = $row;
+		return $page;
+	}
+	
+	public function convertToTemplate() {
+		if ($this->row->scrapeId) {
+			
+			// need to zip the resource folders
+			$zip = new Redokes_Zip();
+			$zipFileName = $this->row->hash . '.zip';
+			$zipFile = $this->getPrivateDir() . $zipFileName;
+			if (is_file($zipFile)) {
+				unlink($zipFile);
+			}
+			$zip->open($zipFile, ZIPARCHIVE::OVERWRITE);
+			
+			$dirsToZip = array();
+			
+			// Read top level items
+			$dir = $this->getPrivateDir();
+			if ($handle = opendir($dir)) {
+				while (false !== ($file = readdir($handle))) {
+					if (substr($file, 0, 1) != '.') {
+						$path = $dir . $file;
+						if (is_dir($path)) {
+							$zip->addDir($path);
+						}
+						else if (is_file($path)) {
+							$zip->addFile($path, $file);
+						}
+					}
+				}
+				closedir($handle);
+			}
+			$zip->close();
+			
+			// Get the first page for this scrape
+			$page = $this->getFirstPage();
+			
+			// Make an index.html file for the top
+			$indexFile = $this->getPrivateDir() . 'index.html';
+			file_put_contents($indexFile, $page->row->content);
+			
+			// Make the template from the html file
+			$newTemplateTitle = $this->row->title . ' (Scrape Import)';
+			
+			$template = new Template_Model_Template();
+			$template->loadRow($newTemplateTitle, 'title');
+			$template->row->title = $newTemplateTitle;
+			if ($template->save()) {
+				$template->processResourceFile($zipFile);
+				$template->processTemplateFile($indexFile);
+				$template->createThumb();
+			}
+			else {
+				$this->addError('Title not unique');
+			}
+			unlink($zipFile);
+			unlink($indexFile);
 		}
 	}
 	
